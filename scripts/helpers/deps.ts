91f102e60cd631782e03023142f1ced3bdac8825
David Sherret <dsherret@gmail.com>
2022-02-25 09:57:52 -0500

    chore: update copyright year

--- scripts/helpers/deps.ts
+++ scripts/helpers/deps.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 
 export * as path from "https://deno.land/std@0.113.0/path/mod.ts";
 export * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
