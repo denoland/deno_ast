c3a1611a151b9db46a9ac06ed09b42f83c951f39
David Sherret <dsherret@gmail.com>
2022-03-21 12:39:34 -0400

    Update scripts.

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,3 +1,3 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 
-export * from "https://raw.githubusercontent.com/denoland/automation/d0261eee119a422ca2ef7ca7e1f0e6282d327903/mod.ts";
+export * from "https://raw.githubusercontent.com/denoland/automation/0.7.0/mod.ts";
