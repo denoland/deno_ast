bb04f8ecfc8cc113a1faa6afecb47abb45590cd8
David Sherret <dsherret@gmail.com>
2022-08-04 11:13:13 -0400

    chore: update denoland/automation

--- scripts/reset.ts
+++ scripts/reset.ts
@@ -5,8 +5,5 @@ import { Repos } from "./repos.ts";
 const repos = await Repos.load({ skipLoadingCrates: true });
 
 if (confirm("Are you sure you want to git reset --hard all the repos?")) {
-  await Promise.all(
-    repos.nonDenoAstRepos()
-      .map((c) => c.gitResetHard()),
-  );
+  await Promise.all(repos.nonDenoAstRepos().map((c) => c.gitResetHard()));
 }
