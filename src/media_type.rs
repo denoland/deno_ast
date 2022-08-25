59758f0c9f55301104e51a906a32b5a1370ff085
David Sherret <dsherret@gmail.com>
2022-08-25 12:41:56 -0400

    Remove needless cfg

--- src/media_type.rs
+++ src/media_type.rs
@@ -496,13 +496,10 @@ mod tests {
       let actual = resolve_url_or_path(specifier);
       assert_eq!(MediaType::from(&actual), expected);
 
-      #[cfg(feature = "module_specifier")]
-      {
-        assert_eq!(
-          MediaType::from_specifier_and_headers(&actual, None),
-          expected
-        );
-      }
+      assert_eq!(
+        MediaType::from_specifier_and_headers(&actual, None),
+        expected
+      );
     }
   }
 
@@ -613,15 +610,12 @@ mod tests {
         expected
       );
 
-      #[cfg(feature = "module_specifier")]
-      {
-        let mut headers = std::collections::HashMap::<String, String>::new();
-        headers.insert("content-type".to_string(), content_type.to_string());
-        assert_eq!(
-          MediaType::from_specifier_and_headers(&specifier, Some(&headers)),
-          expected
-        );
-      }
+      let mut headers = std::collections::HashMap::<String, String>::new();
+      headers.insert("content-type".to_string(), content_type.to_string());
+      assert_eq!(
+        MediaType::from_specifier_and_headers(&specifier, Some(&headers)),
+        expected
+      );
     }
   }
 
