2b598a3d884f469561019e9bdcf1d12b8766e186
David Sherret <dsherret@gmail.com>
2023-10-01 18:16:16 -0400

    Fix

--- src/transpiling/transforms.rs
+++ src/transpiling/transforms.rs
@@ -1,7 +1,5 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
-use swc_ast::Ident;
-
 use crate::swc::ast as swc_ast;
 use crate::swc::common::DUMMY_SP;
 use crate::swc::visit::noop_fold_type;
