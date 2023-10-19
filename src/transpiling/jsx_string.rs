5db8f13b988602aa9b8f3fc5af064c4edb07e684
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-10-19 14:12:20 +0200

    half way through the basic test case

--- src/transpiling/jsx_string.rs
+++ src/transpiling/jsx_string.rs
@@ -1,57 +1,111 @@
-use swc_atoms::{js_word, Atom, JsWord};
+use swc_common::DUMMY_SP;
+// use swc_atoms::{js_word, Atom, JsWord};
 use swc_common::{
-  comments::{Comment, CommentKind, Comments},
-  errors::HANDLER,
-  iter::IdentifyLast,
-  sync::Lrc,
+  //   comments::{Comment, CommentKind, Comments},
+  //   errors::HANDLER,
+  //   iter::IdentifyLast,
+  //   sync::Lrc,
   util::take::Take,
-  FileName, Mark, SourceMap, Span, Spanned, DUMMY_SP,
+  //   FileName, Mark, SourceMap,
+  Span,
+  Spanned,
 };
 use swc_ecma_ast::*;
-use swc_ecma_utils::{
-  drop_span, member_expr, prepend_stmt, private_ident, quote_ident, undefined,
-  ExprFactory,
-};
+// use swc_ecma_utils::{
+//   drop_span, member_expr, prepend_stmt, private_ident, quote_ident, undefined,
+//   ExprFactory,
+// };
 use swc_ecma_visit::{
   as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith,
 };
 
-struct JsxString {}
+struct JsxString {
+  next_index: usize,
+}
+
+impl Default for JsxString {
+  fn default() -> Self {
+    Self { next_index: 0 }
+  }
+}
+
+fn create_tpl_binding_name(index: usize) -> String {
+  format!("$$_tpl_{index}")
+}
+
+impl JsxString {
+  fn generate_tpl_join(&self, el: JSXElement) -> Expr {
+    let name = create_tpl_binding_name(self.next_index);
+    let span = el.span();
+
+    // $$_tpl_1.join("");
+    Expr::Call(CallExpr {
+      span,
+      callee: Callee::Expr(Box::new(Expr::Member(MemberExpr {
+        span: DUMMY_SP,
+        obj: Box::new(Expr::Ident(Ident::new(name.into(), DUMMY_SP))),
+        prop: MemberProp::Ident(Ident::new("join".into(), DUMMY_SP)),
+      }))),
+      args: vec![ExprOrSpread {
+        spread: None,
+        expr: Box::new(Expr::Lit(Lit::Str(Str {
+          span: DUMMY_SP,
+          value: "".into(),
+          raw: None,
+        }))),
+      }],
+      type_args: Default::default(),
+    })
+  }
+}
 
 impl VisitMut for JsxString {
   noop_visit_mut_type!();
+
+  fn visit_mut_module(&mut self, module: &mut Module) {
+    eprintln!("ast {:#?}", module);
+    module.visit_mut_children_with(self);
+  }
 
   fn visit_mut_expr(&mut self, expr: &mut Expr) {
     // let top_level_node = self.top_level_node;
     // let mut did_work = false;
 
-    // if let Expr::JSXElement(el) = expr {
-    //   did_work = true;
-    //   // <div></div> => React.createElement('div', null);
-    //   *expr = self.jsx_elem_to_expr(*el.take());
-    // } else if let Expr::JSXFragment(frag) = expr {
-    //   // <></> => React.createElement(React.Fragment, null);
-    //   did_work = true;
-    //   *expr = self.jsx_frag_to_expr(frag.take());
-    // } else if let Expr::Paren(ParenExpr {
-    //   expr: inner_expr, ..
-    // }) = expr
-    // {
-    //   if let Expr::JSXElement(el) = &mut **inner_expr {
-    //     did_work = true;
-    //     *expr = self.jsx_elem_to_expr(*el.take());
-    //   } else if let Expr::JSXFragment(frag) = &mut **inner_expr {
-    //     // <></> => React.createElement(React.Fragment, null);
-    //     did_work = true;
-    //     *expr = self.jsx_frag_to_expr(frag.take());
-    //   }
-    // }
+    if let Expr::JSXElement(el) = expr {
+      // TODO:
+      // 1. create a `_tpl_<name>` which is an array literal
+      // 2. transform the element into a list of string literals and
+      //    push them to the `_tpl_<name>` literal node
+      // 3. change the `expr` to be `_tpl_<name>.join("");`
+
+      self.next_index += 1;
+
+      //   did_work = true;
+      //   // <div></div> => React.createElement('div', null);
+      *expr = self.generate_tpl_join(*el.take());
+    } else if let Expr::JSXFragment(frag) = expr {
+      //   // <></> => React.createElement(React.Fragment, null);
+      //   did_work = true;
+      //   *expr = self.jsx_frag_to_expr(frag.take());
+    } else if let Expr::Paren(ParenExpr {
+      expr: inner_expr, ..
+    }) = expr
+    {
+      if let Expr::JSXElement(el) = &mut **inner_expr {
+        // did_work = true;
+        // *expr = self.jsx_elem_to_expr(*el.take());
+      } else if let Expr::JSXFragment(frag) = &mut **inner_expr {
+        // <></> => React.createElement(React.Fragment, null);
+        // did_work = true;
+        // *expr = self.jsx_frag_to_expr(frag.take());
+      }
+    }
 
     // if did_work {
     //   self.top_level_node = false;
     // }
 
-    // expr.visit_mut_children_with(self);
+    expr.visit_mut_children_with(self);
 
     // self.top_level_node = top_level_node;
   }
@@ -80,10 +134,13 @@ mod tests {
   #[test]
   fn basic_test() {
     test_transform(
-      JsxString {},
-      r#"const a = <div>Hello!</div>;"#,
-      r#"const _tpl = ["<div>Hello!</div>"];
-const a = _tpl.join("");"#,
+      JsxString::default(),
+      r#"const a = <div>Hello!</div>;
+const b = <div>Hello!</div>;"#,
+      r#"const $$_tpl_1 = ["<div>Hello!</div>"];
+const $$_tpl_2 = ["<div>Hello!</div>"];
+const a = $$_tpl_1.join("");
+const b = $$_tpl_2.join("");"#,
     );
   }
 
@@ -107,7 +164,7 @@ const a = _tpl.join("");"#,
     );
     let input = StringInput::from(&*source_file);
     let syntax = Syntax::Typescript(TsConfig {
-        tsx: true,
+      tsx: true,
       ..Default::default()
     });
     let mut parser = Parser::new(syntax, input, None);
