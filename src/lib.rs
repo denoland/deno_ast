b91b3a5a52fc397f82ea118f1507fe40f817a941
David Sherret <dsherret@users.noreply.github.com>
2023-11-16 09:25:55 -0500

    fix: add back accidentally removed export for swc::dep_graph (#192)

--- src/lib.rs
+++ src/lib.rs
@@ -48,6 +48,8 @@ pub mod swc {
   pub use swc_ecma_ast as ast;
   #[cfg(feature = "codegen")]
   pub use swc_ecma_codegen as codegen;
+  #[cfg(feature = "dep_graph")]
+  pub use swc_ecma_dep_graph as dep_graph;
   pub use swc_ecma_parser as parser;
   #[cfg(feature = "transforms")]
   pub mod transforms {
